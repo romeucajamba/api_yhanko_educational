9423fd767ab74f5ea41be0460288e3a876eaac7c
romeucajamba <romeucajamba@gmail.com>
2024-09-17 00:22:49 +0100

    chore:add hook in all routes

---
+++ src/routes/notes.routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from "fastify";
+import { verifyJWT } from "@/middlewares/verify-jwt";
+
+export async function notesRoutes(app: FastifyInstance) {
+        //Permite que só os usuários autenticados vão conseguir chamar as rotas
+    app.addHook('onRequest', verifyJWT)
+}
